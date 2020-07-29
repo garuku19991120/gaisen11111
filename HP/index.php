75462f268a881a30b31ad624176385fd677872d0
G <68621876+garuku19991120@users.noreply.github.com>
2020-07-29 16:55:26 +0900

    Add files via upload

---
+++ HP/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
+    <meta http-equiv="refresh" content="5; URL=">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>HOME</title>
+</head>
+<body>
+  <center>
+
+<div class="zentai2">
+    <table class="zentai" border="1">
+      <tr>
+        <td class="buttontable">
+          <table class="none">
+            <tr>
+              <td>
+                <div class="container">
+                  <a href="index.php" class="btn-animation-02">
+                    <span>HOME<span></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="container">
+                  <a href="profile.php" class="btn-animation-02">
+                    <span>PROFILE<span></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="container">
+                  <a href="works.php" class="btn-animation-02">
+                    <span>WORKS<span></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="container">
+                  <a href="contact.php" class="btn-animation-02">
+                    <span>CONTACT<span></a>
+                </div>
+              </td>
+            </tr>
+          </table><!-- ボタンが格納されている。 -->
+        </td>
+        <td>
+          <table class="main_contents">
+            <tr>
+              <td></td>
+            </tr>
+          </table>
+          <td>
+            <table class="yohaku">
+              <tr>
+                <td></td>
+              </tr>
+            </table>
+          </td>
+        </td>
+      </tr>
+    </table>
+</div>
+<hr>
+<div class="copywrite">
+  <p>© 2020-<?php $today = date("Y");print_r($today);?> GAISEN</p>
+</div>
+</body>
+</html>
