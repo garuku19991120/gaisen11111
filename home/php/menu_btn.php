bcae05feb29d0a7be593b3e5f5ef869ad9e2ecf0
t-1120 <r_togai@itccl.jp>
2020-08-11 10:31:56 +0900

    menubtnのファイルを作った

---
+++ home/php/menu_btn.php
@@ -0,0 +1,23 @@
+<?php
+echo '
+<div class="manu_btn">
+  <ul>
+    <li>
+      <button class="menu_button"><a href="" class="menu">1</a></button>
+    </li>
+    <li>
+      <button class="menu_button"><a href="" class="menu">2</a></button>
+    </li>
+    <li>
+      <button class="menu_button"><a href="" class="menu">3</a></button>
+    </li>
+    <li>
+      <button class="menu_button"><a href="" class="menu">4</a></button>
+    </li>
+  </ul>
+</div>
+
+
+';
+
+ ?>
