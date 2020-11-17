adcf43ccfd33e1a80166ad70a30c7b054f995ccd
t-1120 <r_togai@itccl.jp>
2020-11-17 15:14:56 +0900

    各ページを配置

--- HP/index.php
+++ HP/index.php
@@ -7,12 +7,18 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <link rel="stylesheet" href="./css/master.min.css">
-  <title>test</title>
+  <link rel="stylesheet" href="./css/menu.min.css">
+  <title>GAISEN</title>
 </head>
 <body>
-<?php include('./php/menu.php'); ?>
-<?php
-  echo("yeah");
-?>
+<table>
+  <tr>
+    <td class="m_space">
+      <?php include('./php/menu.php'); ?>
+    </td>
+    <td class="main_space">
+    </td>
+  </tr>
+</table>
 </body>
 </html>
---
+++ HP/index2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TweenMax.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <link rel="stylesheet" href="./css/master.min.css">
+  <link rel="stylesheet" href="./css/menu.min.css">
+  <title>CONTACT</title>
+</head>
+<body>
+<table>
+  <tr>
+    <td class="m_space">
+      <?php include('./php/menu.php'); ?>
+    </td>
+    <td class="main_space">
+    </td>
+  </tr>
+</table>
+</body>
+</html>
